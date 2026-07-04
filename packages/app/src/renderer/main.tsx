ad148a6592530cdc5c078c89c22d4b73abd930d2
lizhiyu022 <lizhiyu022@ke.com>
2026-07-04 17:12:43 +0800

    fix: remove unused StrictMode import

--- packages/app/src/renderer/main.tsx
+++ packages/app/src/renderer/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
@@ -6,8 +5,4 @@ import { App } from "./App";
 
 document.documentElement.dataset.platform = window.electronAPI.platform;
 
-createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <App />,
-  // </StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
